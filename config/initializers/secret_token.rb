d12da1024867c1b1fbdcd32ce8e49f1c181fed0b
Anthony Williams <anthony@madebypurple.com>
2011-05-03 06:52:00 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'e4b8bcf0eede73ff15ed758d2fd6015853b43694d6a75097ab6ac35a9df0289e091dd42602fc76da77ce785d57fec301e798ba7a452136895083335dbf793c16'
