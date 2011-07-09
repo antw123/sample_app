b1d51e550d98bcfd2e56b7c35aeff3c046a1e7a4
Anthony Williams <anthony@madebypurple.com>
2011-07-08 20:11:40 -0400

    Finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,15 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @title = @user.name
   end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the Sample App"
+      redirect_to user_path(@user)
+    else
+      @title = "Sign up"
+      render 'new'
+    end
+  end
 end
