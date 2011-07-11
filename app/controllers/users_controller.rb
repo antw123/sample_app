70be45721d1e007496bfb899cfff830715a96f29
Anthony Williams <anthony@madebypurple.com>
2011-07-11 13:58:49 -0400

    Friendly fowarding and correct user requirment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
   
   def new
     @user = User.new
@@ -40,7 +41,11 @@ class UsersController < ApplicationController
   private
     
     def authenticate
-      flash[:notice] = "Please Sign in to access this page."
-      redirect_to signin_path unless signed_in?
+      deny_access unless signed_in?
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
     end
 end
